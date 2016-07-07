d5b09f29e3f7dc48e5611c2b812c7352e6d88ee3
Brij <kishorsharma7@gmail.com>
2016-07-07 20:56:23 +0530

    First component

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by brij on 5/7/16.
+ */
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'my-app',
+    template: `<div>Hello Angular 2!!!</div>`
+})
+export class AppComponent{}
